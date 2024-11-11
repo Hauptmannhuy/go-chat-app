e83e3991a696fc242dc8cfa3b48b9974a2513f98
hauptmannhuy <dindago2324@gmail.com>
2024-11-11 23:33:19 +0200

    concurrency tests for chat management

--- server/chat_test.go
+++ server/chat_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -18,6 +19,33 @@ func TestCreateChat(t *testing.T) {
 			t.Errorf("Fail")
 		}
 	})
+}
+
+func TestConcurrentCreateChat(t *testing.T) {
+	wg := sync.WaitGroup{}
+	var chatList ChatList
+	chatList.Chats = make(map[string]*Chat)
+	chatNames := []string{"cool chat", "foo", "mooze", "stalker", "4335", "grAWSRW", "GRERHAS", "h3af3"}
+
+	for i := 0; i < len(chatNames); i++ {
+		name := chatNames[i]
+		wg.Add(1)
+		go func() {
+			chatList.CreateChat(name)
+			wg.Done()
+		}()
+		wg.Wait()
+	}
+
+	for _, key := range chatNames {
+		_, exists := chatList.Chats[key]
+		if !exists {
+			t.Errorf("Error during concurrent chat creation, chat '%s' must exist", key)
+		}
+	}
+	if len(chatNames) != len(chatList.Chats) {
+		t.Errorf("Error during concurrent chat creation, length of chat capacity should be %d, instead got %d", len(chatNames), len(chatList.Chats))
+	}
 }
 
 type MockedDatabase struct{}
@@ -68,4 +96,51 @@ func TestAddClientToSubRooms(t *testing.T) {
 	if expectedCapacity != actualCapacity {
 		t.Errorf("Expected chat to have %d members, but got %d", expectedCapacity, actualCapacity)
 	}
+}
+
+func TestConcurrentAddClientToSubRooms(t *testing.T) {
+	var chatList ChatList
+	chatList.Chats = map[string]*Chat{"chat 1": &Chat{ID: "chat 1"}}
+	wg := sync.WaitGroup{}
+	clients := []*Client{}
+	names := []string{"joe", "bill", "grigorovich", "mitya"}
+	sub := []string{"chat 1"}
+	for i := 0; i < len(names); i++ {
+		clients = append(clients, &Client{
+			username: names[i],
+			subs:     sub,
+		})
+	}
+
+	for _, client := range clients {
+		wg.Add(1)
+		go func(client *Client) {
+			defer wg.Done()
+			chatList.addClientToSubRooms(client)
+		}(client)
+	}
+	wg.Wait()
+
+	specifiedChat := chatList.Chats["chat 1"]
+	if len(specifiedChat.members) != len(names) {
+		t.Errorf("Expected %d members, got %d", len(names), len(specifiedChat.members))
+	}
+
+	for _, name := range names {
+
+		var contains = func(name string) bool {
+			for _, cl := range specifiedChat.members {
+				if cl.username == name {
+					return true
+				}
+			}
+			return false
+		}
+		ok := contains(name)
+
+		if !ok {
+			t.Errorf("Expected chat to contain member with username %s", name)
+		}
+	}
+
 }
